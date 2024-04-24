f326b10ed703fa91946ce53676b9f33ace7f4e1b
syedfakhar25 <44221977+syedfakhar25@users.noreply.github.com>
2024-04-24 10:45:34 +0500

    animals data in settings

--- app/Http/Controllers/AllDataController.php
+++ app/Http/Controllers/AllDataController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class AllDataController extends Controller
 {
     public function allData(){
-        $districts = DB::select('SELECT * FROM districts');
+        /*$districts = DB::select('SELECT * FROM districts');
         $divisions = DB::select('SELECT * FROM divisions');
         $villages = DB::select('SELECT * FROM villages');
         $tehsils = DB::select('SELECT * FROM tehsils');
@@ -18,7 +18,73 @@ class AllDataController extends Controller
         $species = DB::select('SELECT * FROM species');
         $productions = DB::select('SELECT * FROM productions');
         $species_productions = DB::select('SELECT * FROM species_productions');
-        $breeds = DB::select('SELECT * FROM breeds');
+        $breeds = DB::select('SELECT * FROM breeds');*/
+        // Fetch data for districts, divisions, villages, tehsils, and councils
+        $districts = $divisions = $villages = $tehsils = $councils = [];
+        $districtsResult = DB::select('SELECT * FROM districts');
+        $divisionsResult = DB::select('SELECT * FROM divisions');
+        $villagesResult = DB::select('SELECT * FROM villages');
+        $tehsilsResult = DB::select('SELECT * FROM tehsils');
+        $councilsResult = DB::select('SELECT * FROM councils');
+
+// Organize district data into key-value pairs
+        foreach ($districtsResult as $district) {
+            $districts[$district->id] = $district->name;
+        }
+
+// Organize division data into key-value pairs
+        foreach ($divisionsResult as $division) {
+            $divisions[$division->id] = $division->name;
+        }
+
+// Organize village data into key-value pairs
+        foreach ($villagesResult as $village) {
+            $villages[$village->id] = $village->name;
+        }
+
+// Organize tehsil data into key-value pairs
+        foreach ($tehsilsResult as $tehsil) {
+            $tehsils[$tehsil->id] = $tehsil->name;
+        }
+
+// Organize council data into key-value pairs
+        foreach ($councilsResult as $council) {
+            $councils[$council->id] = $council->name;
+        }
+
+// Fetch data for species, productions, species_productions, and breeds
+        $species = $productions = $speciesProductions = $breeds = [];
+        $speciesResult = DB::select('SELECT * FROM species');
+        $productionsResult = DB::select('SELECT * FROM productions');
+        $speciesProductionsResult = DB::select('SELECT * FROM species_productions');
+        $breedsResult = DB::select('SELECT * FROM breeds');
+
+// Organize species data into key-value pairs
+        foreach ($speciesResult as $specie) {
+            $species[$specie->id] = $specie->name;
+        }
+
+// Organize production data into key-value pairs
+        foreach ($productionsResult as $production) {
+            $productions[$production->id] = $production->name;
+        }
+
+// Organize species productions data into key-value pairs
+        foreach ($speciesProductionsResult as $speciesProduction) {
+            // Assuming species_productions table has a unique id field
+            $speciesProductions[$speciesProduction->id] = [
+                'species_id' => $speciesProduction->species_id,
+                'production_id' => $speciesProduction->production_id
+            ];
+        }
+
+// Organize breed data into key-value pairs
+        foreach ($breedsResult as $breed) {
+            $breeds[$breed->id] = $breed->name;
+        }
+
+// Now you have organized data in key-value pairs for each category
+
 
         return response()->json(
             [
