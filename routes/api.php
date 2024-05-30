a231d218ff2087059c64656f3c3de0e5fa24e887
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2024-05-30 15:15:31 +0500

    testing

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
-// Route::get('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::get('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
