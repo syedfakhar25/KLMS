16bf9e364681ca64048401af66b1bd1a03fa3c7b
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2024-05-31 15:53:32 +0500

    testing

--- app/Http/Controllers/PremesisController.php
+++ app/Http/Controllers/PremesisController.php
@@ -40,14 +40,10 @@ class PremesisController extends Controller
      */
     public function store(Request $request)
     {
-        return response()->json(
-            ['premises' => $request->all()],
-            200);
 
 
 
 
-            
         $premise = premesis::create($request->all());
         if ($request->hasFile('image')) {
               // Get the image file from the request
