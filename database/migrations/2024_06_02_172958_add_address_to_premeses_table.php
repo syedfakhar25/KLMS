460d88a843a2c64ea0c405583e8396a855cc56bc
Adeel Cheema <adeelcheema2011@gmail.com>
2024-06-02 22:33:15 +0500

    address added in table

--- database/migrations/2024_06_02_172958_add_address_to_premeses_table.php
+++ database/migrations/2024_06_02_172958_add_address_to_premeses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('premeses', function (Blueprint $table) {
-            $table->integer('address')->nullable();
+            $table->string('address')->nullable();
         });
     }
 
