5ba55049fe63ff078468bf3fe2787992fc25ec52
Adeel Cheema <adeelcheema2011@gmail.com>
2024-06-10 12:24:01 +0500

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-// use App\Models\User;
-use http\Client\Curl\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 class AuthController extends Controller
