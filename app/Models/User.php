87ccc8ab5a4edc2faaa3ac10512cd5a42c72b6e6
Adeel Cheema <adeelcheema2011@gmail.com>
2024-06-10 12:41:17 +0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'otp_expires_at' => 'datetime',
     ];
 
     public function premises(){
