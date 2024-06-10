7eb7a55e671ee2efc4829abd545cafb7b37f34b8
Adeel Cheema <adeelcheema2011@gmail.com>
2024-06-10 12:37:48 +0500

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,18 +31,13 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-        ]);
-
+  
         $otp = rand(100000, 999999); // Generate a 6-digit OTP
 
         $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
             'otp' => $otp,
             'otp_expires_at' => Carbon::now()->addMinutes(10)
         ]);
