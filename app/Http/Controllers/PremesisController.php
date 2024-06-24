ecae272edf79616b16536cb357ef7fbb26346d46
Adeel Cheema <adeelcheema2011@gmail.com>
2024-06-24 15:43:02 +0500

    Update PremesisController.php

--- app/Http/Controllers/PremesisController.php
+++ app/Http/Controllers/PremesisController.php
@@ -18,7 +18,7 @@ class PremesisController extends Controller
     {
         $user = Auth::user();
         $premises = Premesis::where('user_id', $user->id)
-                    ->orderBy('created_at', 'asc') 
+                    ->orderBy('created_at', 'desc') 
                     ->get();    
     
         return response()->json(
