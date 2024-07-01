2568dfbc0112dbac51c55853edd7fe223f4fbfc2
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2024-07-01 17:56:42 +0500

    'vacanations'

--- database/migrations/2024_07_01_123513_add_animals_to_vaccinations_table.php
+++ database/migrations/2024_07_01_123513_add_animals_to_vaccinations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('vaccinations', function (Blueprint $table) {
-            //
+            $table->text('animals')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('vaccinations', function (Blueprint $table) {
-            //
+            $table->dropColumn('animals');
         });
     }
 };
