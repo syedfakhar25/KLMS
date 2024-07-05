316f4dba13790dd3494b9ba853a3a0355084f563
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2024-07-05 16:55:32 +0500

    push

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'title' => 'Admin',
+            'description' => 'AJK Administrator role',
+        ]);
+        Role::create([
+            'title' => 'User',
+            'description' => 'AJK Administrator role',
+        ]);
+        Role::create([
+            'title' => 'District Admin',
+            'description' => 'District Administrator role',
+        ]);
+        Role::create([
+            'title' => 'UC Admin',
+            'description' => 'UC Administrator role',
+        ]);
+        Role::create([
+            'title' => 'Village Admin',
+            'description' => 'Village Administrator role',
+        ]);
+    }
+}
