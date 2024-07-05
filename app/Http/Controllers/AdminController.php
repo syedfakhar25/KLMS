ddcc5b847bf1a9c897251d58b8773b4d250bfea6
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2024-07-05 12:29:06 +0500

    Scheme hoti ha

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,29 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Animal;
+use App\Models\Disease;
 use App\Models\Premesis;
+use App\Models\Specie;
+use App\Models\Vaccination;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function dashboard() {
-        
+        $premesis= Premesis::all();
         if (auth()->user()->role_id == 1) {
-            $premesis = Premesis::all();
+            if(auth()->user()->village){
+                $premesis= $premesis->where('village',auth()->user()->village);
+            }
+            $premesis_id=$premesis->pluck('id');
+            $animals =Animal::wherein('premesis_id', $premesis_id); 
+            $vaccinations =Vaccination::wherein('premesis_id', $premesis_id); 
+            $diseases =Disease::wherein('premesis_id', $premesis_id); 
+            $species =Specie::wherein('premesis_id', $premesis_id); 
+            // $premesis = Premesis::where(['province'=>, 	'district'=>, 'tehsil'=>,'uc'=>, 'village'=>]);
         }
 
         return response()->json(
             [
-                'premises' => $premesis,
-                'animals' => 20,
-                'vaccination' => 30,
-                'labtest' => 20,
-                'dieses' => 50,
-                'species' => 90,
-                'productions' => 90,
-                'species_productions' => 90,
-                'breeds' => 90,
-
+                'premises' => $premesis->count(),
+                'animals' => $animals->count(),
+                'vaccination' => $vaccinations->count(),
+                'labtest' => '-',
+                'dieses' => $diseases->count(),
+                'species' => $species->count(),
+                'breeding' => '-',
+                'birth' => '-',
+                'movment' => '-',
+                'quarantine' => '-',
+                'slaughtered' => '-',
+                'exported' => '-',
             ],
             200);
     }
